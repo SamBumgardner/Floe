9fbe6f6f217a0c5e125b3619ec26fef9d5a360bf
Sam Bumgardner <sambumgardner1@gmail.com>
2016-03-07 16:24:25 -0600

    Fixed a mistake in the Obstacle class, and added the proper sprite.

--- Floe/src/entities/Obstacle.hx
+++ Floe/src/entities/Obstacle.hx
@@ -7,8 +7,9 @@ import com.haxepunk.HXP; //for debug;
 
 class Obstacle extends Entity{
 	
-	public override function new(){
-		graphic = new Image("graphics\block.png");
+	public override function new(x:Int, y:Int){
+		super(x,y);
+		graphic = new Image("graphics/groundRock.png");
 		setHitbox(32, 32);
 		type = "obstacle";
 	}
