21d18e38586872cbfa2d91f08792fca7f003f145
Sam Bumgardner <sambumgardner1@gmail.com>
2016-03-12 12:53:30 -0600

    Rewrote GameScene to use static variables for shared data. Expects GameManager to be a child of the entity class. Returns the GameManager class when game over occurs.

--- Floe/src/scenes/GameScene.hx
+++ Floe/src/scenes/GameScene.hx
@@ -6,34 +6,49 @@ import entities.Player;
 import entities.WaterTile;
 import entities.GroundTile;
 import entities.Obstacle;
+import entities.GameManager;
+
 import com.haxepunk.Sfx;
 
 
 class GameScene extends Scene
 {
+	//declare static entities 
+	private static var music:Sfx;
+	public  static var GM:GameManager;
 	
-	private var music:Sfx;
+	//Use a single boolean variable to check if the static entities and assets have been set up.
+	private static var staticSetup:Bool = false;
 	
-	public function new()
+	public function new(?gameManager:GameManager) //leading ? means optional parameter.
 	{
 		super();
-	}
-	public override function begin()
-	{
-		HXP.console.log(["Level is loading..."]);
 		
-		music = new Sfx("audio/bgm.mp3");
 		music.loop();
+		if(!staticSetup){
+			//All entity and asset assignments for static variables go here.
+			
+			music = new Sfx("audio/bgm.mp3");
+			GM = gameManager;
+			
+			staticSetup = true;
+		}
 		
-		var dimensionX = 10;
-		//var dimensionY = 20
-		var placeX = 32;
+	}
+	
+	private function generateLevel(){
+		var dimensionX = 10; // max width of the generated level area.
+		//var dimensionY = 8;   not used at the moment.
+		
+		var placeX = 32; //Starting X & Y position for placing tiles.
 		var placeY = 32;
-		var playerX = 0;
+		
+		var playerX = 0; //Player's starting X & Y position.
 		var playerY = 0;
+		
 		var numOfTiles = 60; //controls number of water tiles initially placed.
 		
-		var PC = new Player(playerX, 0);
+		var PC = new Player(playerX, playerY);
 		add(PC);
 		
 		while(numOfTiles > 0){
@@ -41,7 +56,7 @@ class GameScene extends Scene
 				add(new WaterTile(placeX, placeY));
 				numOfTiles -= 1;
 				
-				HXP.engine.gm.waterAdded();
+				GM.waterAdded();
 			}
 			else{
 				if(HXP.random > .5){
@@ -57,11 +72,35 @@ class GameScene extends Scene
 				placeY +=32;
 			}
 		}
+		
 		HXP.console.log(["Level has been generated!"]);
 	}
+	
+	public override function begin()
+	{
+		HXP.console.log(["Level is loading..."]);
+		
+		if(!musicPlaying){
+			music.loop();
+		}
+		
+		add(GM);
+		
+		generateLevel();
+		
+	}
+	
 	public override function end(){
 	
 		music.stop();
+		removeAll();
 	
+	}
+	
+	//Called when going to the gameOver scene, and not the next level.
+	// Stops the looping music.
+	// Returns the GameManager object for the gameOver scene to use.
+	public function gameOver(){
+		return GM;
 	}
 }
