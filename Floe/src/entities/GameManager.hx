9f39c76bcc749cc581344f343b150c1f6e6e9cc8
Sam Bumgardner <sambumgardner1@gmail.com>
2016-03-22 21:29:35 -0500

    Moved player health tracking to the GameManager

--- Floe/src/entities/GameManager.hx
+++ Floe/src/entities/GameManager.hx
@@ -11,20 +11,20 @@ import com.haxepunk.Entity;
 
 class GameManager extends Entity{
 	
-	private var unfrozenWaterCount:Int;
-	private var totalScore:Int;
+	private var unfrozenWaterCount:Int 	= 0;
+	private var totalScore:Int 			= 0;
+	private var playerHealth:Int 		= 3;
 	
 	public function new(x:Int = 0, y:Int = 0){
 		super(x, y);
-		
-		unfrozenWaterCount = 0;
-		totalScore = 0;
-		//I need to use the random seed for something.
 	}
 	
+	//Called by WaterTile upon construction
 	public function waterAdded(){
 		unfrozenWaterCount++;
 	}
+	
+	//Called by WaterTile as part of its freeze() function
 	public function waterFrozen(){
 		unfrozenWaterCount--;
 		addScore(10);
@@ -37,16 +37,21 @@ class GameManager extends Entity{
 		}
 	}
 	
+	public function damagePlayer(damage:Int){
+		playerHealth -= damage;
+		HXP.console.log(["Took ", damage, " damage! Only ", playerHealth, " health remaining."]);
+		if(playerHealth <= 0){
+			HXP.engine.gameOver();
+		}
+	}
+	
+	//Called by various entities, increases score
 	public function addScore(points:Int){
 		totalScore += points;
 	}
 	
+	//Returns the player's score as an integer
 	public function getScore(){
 		return totalScore;
 	}
-	
-	public function newLevelSetup(){
-		unfrozenWaterCount = 0;
-	}
-	
 }
