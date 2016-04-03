e5d3663336de8b748ba217aa531803031e55da27
Sam Bumgardner <sambumgardner1@gmail.com>
2016-04-03 17:10:38 -0500

    Wrote recursive neighbor-counting component.

--- Floe/src/entities/WaterTile.hx
+++ Floe/src/entities/WaterTile.hx
@@ -17,6 +17,8 @@ import com.haxepunk.Entity;
 import scenes.GameScene;
 import entities.GameManager;
 
+import entities.MovingActor; //This is just for the Direction enum
+
 
 class WaterTile extends Tile {
 	static public var commonImage:Image;
@@ -54,6 +56,104 @@ class WaterTile extends Tile {
 		frozen = true;
 		scenes.GameScene.GM.waterFrozen();
 	}
+	
+	
+	///////////////////////////////////////////
+	//          AUTOMATIC  FREEZING          //
+	///////////////////////////////////////////
+	
+	
+	// checkTile(e:Entity)
+	//
+	// Helper function for checkUnfrozenNeighbors
+	// Identifies if e is an unfrozen water tile.
+	//
+	// Note: e MUST be castable to the WaterTile class!
+	// 		 If not, we get silent, undefined behavior.
+	
+	
+	private function checkNeighbor(){
+		if( e != null ){
+			if( (cast e).isFrozen() == false ){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	
+	
+	// checkUnfrozenNeighbors( parentNeighbors:Int, parentDirection:Direction, minimumUnfrozen:Int )
+	//
+	// Loops over the Direction enum to go through adjacent tiles. Pretty cool stuff.
+	//
+	// starts with parent's neighbors, adds 1 for each unfrozen child, then recursively calls for children to add their number of neighbors.
+	// if the number of neighbors ever exceeds minimumUnfrozen, you break out and return num_of_neighbors.
+	
+	// When parents receive children number back, have to subract their own number, then add it to their total.
+	
+	checkUnfrozenNeighbors( parentNeighbors:Int, parentDirection:Direction, minimumUnfrozen:Int ){
+	
+		var numOfNeighbors = parentNeighbors;
+		var neighborWaterTiles = [];
+		var neighborWaterTilesDirections = [];
+		
+		
+		// Check which neighboring tiles are unfrozen water tiles (not including the parentDirection)
+		
+		for( direction in Type.allEnums(Direction) ){ // Performs this loop once for each direction.
+		
+			if (direction == None || direction == parentDirection) { continue; }
+			else{
+			
+				switch direction{
+					case Up:	var e:Entity = collide("waterTile", x, y - size);
+					case Down:	var e:Entity = collide("waterTile", x, y + size);
+					case Left:  var e:Entity = collide("waterTile", x - size, y);
+					case Right: var e:Entity = collide("waterTile", x + size, y); 
+				}
+				
+				if( checkNeighbor( e ) ){
+					neighborWaterTiles.push( e );
+					neighborWaterTilesDirections.push(direction);
+					numOfNeighbors++;
+					
+					if( numOfNeighbors >= minimumUnfrozen){
+						return numOfNeighbors;
+					}
+				}
+			}
+		}
+		
+		
+		var myNumOfNeighbors = numOfNeighbors;
+		
+		var i = 0; 	//index moving through neighborWaterTilesDirections
+		
+		var parentingDirection:Direction; // the value to be passed along as parentDirection in recursion.
+		
+		// Recursively call this function on the unfrozen neighbors, adding results along the way.
+		
+		for (neighbor in neighborWaterTiles){
+			switch neighborWaterTilesDirections[i]{
+				case Up: 	parentingDirection = Down;
+				case Down:  parentingDireciton = Up;
+				case Left:	parentingDirection = Right;
+				case Right:	parentingDirection = Left;
+			}
+		
+			numOfNeighbors += (cast neighbor).checkUnfrozenNeighbors( myNumOfNeighbors, parentingDirection, minimumUnfrozen) - myNumOfNeighbors;
+			if( numOfNeighbors >= minimumUnfrozen){
+						return numOfNeighbors;
+			}
+			i++;
+		}
+		
+		return numOfNeighbors;
+	}
+	
+	
+	
 	public function autoFreezeCheck()
 	{
 		/*This is going to figure out if the water is supposed to freeze over.*/
