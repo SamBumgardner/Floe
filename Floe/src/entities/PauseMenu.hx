ca58f4b4c66ac9364a2194fdd417a012670c5d6c
Sam Bumgardner <sambumgardner1@gmail.com>
2016-04-17 11:23:25 -0500

    Created the PauseMenu entity, which borrows heavily from MenuSelector.hx. Ideally, they would inherit from a common class, but that's not top priority at the moment.

---
+++ Floe/src/entities/PauseMenu.hx
@@ -0,0 +1,128 @@
+package entities;
+
+import com.haxepunk.Entity;
+import com.haxepunk.graphics.Image;
+import com.haxepunk.graphics.Text;
+import com.haxepunk.utils.Input;
+import com.haxepunk.utils.Key;
+import com.haxepunk.Sfx;
+import com.haxepunk.HXP;
+import scenes.GameScene;
+
+class PauseMenu extends Entity
+{
+	
+	private static var moveDistance:Int = 50;
+	private static var verticalMove:Int = 0;
+	
+	private static var menuMove:Sfx;
+	private static var menuPaused:Sfx;
+	private static var menuUnpaused:Sfx;
+	
+	private static var assetsInitialized;
+	
+	private static var containerImg:Image;
+	private static var selectorImg:Image;
+	private static var pausedTextImg:Text;
+	private static var resumeTextImg:Text;
+	private static var quitTextImg:Text;
+	
+	private var containerEnt:Entity;
+	private var pausedTextEnt:Entity;
+	private var resumeTextEnt:Entity;
+	private var quitTextEnt:Entity;
+	
+	private var currentPos:Int; //Used to determine what to do when the player presses spacebar
+	private var numOfPos:Int; //Used to determine the boundaries of the selector's movement.
+
+	public function new(){
+		super(265, 300);
+		
+		layer = -1000; //Must always be at the very front.
+		
+		if( assetsInitialized == false ){
+			menuMove = new Sfx("audio/menuMove.mp3");
+			menuPaused = new Sfx("audio/pause.mp3");
+			menuUnpaused = new Sfx("audio/unpause.mp3");
+			
+			containerImg = new Image("graphics/pauseMenu.png");
+			selectorImg = new Image("graphics/MenuSelector.png");
+			
+			pausedTextImg = new Text("PAUSED");
+			pausedTextImg.setTextProperty("size", 48);
+			resumeTextImg = new Text("Resume");
+			resumeTextImg.setTextProperty("size", 24);
+			quitTextImg = new Text("Quit");
+			quitTextImg.setTextProperty("size", 24);
+			
+			assetsInitialized = true;
+		}
+		graphic = selectorImg;
+		
+		
+	}
+	
+	public override function added(){
+
+		containerEnt = HXP.scene.addGraphic(containerImg, -999, 160, 220);
+		pausedTextEnt = HXP.scene.addGraphic(pausedTextImg, -1000, 320 - (pausedTextImg.width)/2, 230);
+		resumeTextEnt = HXP.scene.addGraphic(resumeTextImg, -1000, 300 , 300);
+		quitTextEnt = HXP.scene.addGraphic(quitTextImg, -1000, 300 , 350);
+		currentPos = 1;
+		numOfPos = 2;
+		y = 300;
+		menuPaused.play(.5);
+		super.added();
+	}
+	
+	private function selectOption(){
+		// 1 = Resume Game
+		// 2 = Quit
+		// Start selected option
+		switch currentPos{
+			case 1:{
+				menuUnpaused.play(.5);
+				(cast HXP.scene).unpauseGame();
+			}
+			case 2: HXP.engine.gameOver();
+		}
+
+	}
+	
+
+	
+
+	
+	public override function update(){
+		// Check for user input: UP, DOWN
+		if (Input.pressed(Key.UP)){ verticalMove--; }
+		if (Input.pressed(Key.DOWN)){ verticalMove++; }
+		// Calculate the next move location
+		var tempPos:Int = currentPos + verticalMove;
+
+		// If selector can move and location is valid, move the selector and play a SFX.
+		if( (0 != verticalMove)
+			&& (0 < tempPos)
+			&& (tempPos <= numOfPos)){
+				moveBy(0, verticalMove * moveDistance);
+				currentPos = tempPos;
+				menuMove.play(.5);
+		}
+		verticalMove = 0;
+		
+		// Check for user input: SPACE, ENTER
+		if(Input.pressed(Key.SPACE) || Input.pressed(Key.ENTER)){
+			selectOption();
+		}
+			
+		super.update();
+	}
+		
+	public override function removed(){
+		HXP.scene.remove( containerEnt );
+		HXP.scene.remove( resumeTextEnt );
+		HXP.scene.remove( quitTextEnt );
+		HXP.scene.remove( pausedTextEnt );
+		super.removed();
+	}
+}
