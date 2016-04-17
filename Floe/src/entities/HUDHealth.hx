784be0288c83048249702caa8317e23bc994ca1b
Noah Bumgardner <noah@playgamecafe.com>
2016-04-16 20:51:40 -0500

    Added heavily commented code for `HUDHealth.hx`. Has most variables needed, except maybe for a temporary-heart-string-value variable inside of setHealth(). Added curse functions to help the HUD choose between showing a red heart or a cursed green heart.

--- Floe/src/entities/HUDHealth.hx
+++ Floe/src/entities/HUDHealth.hx
@@ -9,22 +9,49 @@ import com.haxepunk.graphics.Image;
 
 class HUDHealth extends Entity
 {
-	// Initialize heart image
+	// Initialize:  images, width, margins, and curse status.
+	// imgBG:  Same color as the background. Replaces hearts when at low health.
+	private var imgBG = new Image("graphics/bg_gray.png");
 	private var imgHeart = new Image("graphics/heart.png");
+	private var imgHeartGreen = new Image("graphics/heartGreen.png");
+	private var imgWidth:Int = 32;
+	// imgMargin:  Pixels of space between hearts.
+	private var imgMargin:Int = 4;
+	// curse:  While true, use 'imgHeartGreen'. While false, use 'imgHeart'.
+	private var curse:Bool = false;
 
 	// Constructor
+	// Q: Should the constructor require a starting amount of health?
+	// A:
 	public function new(x:Int, y:Int){
 		super(x, y);
 		initialize();
 	}
 
 	private function initialize():Void{
-		//addGraphic(imgHeart);
-		/*
-		// Idea: Create the 0-5 hearts with a for-loop.
-		for (val in 0..5){
-			addGraphic(imgHeart);
+		// INSERT CODE HERE
+	}
+
+	public setHealth(health:Int){
+		// TODO: Check if cursed,
+		//   then set current heart image string,
+		//   then set/add images.
+		for (index in 0..health){
+			if (index < health) {
+				// TODO: Set/Add heart image
+				;
+			} else {
+				// TODO: Set/Add bg image
+				;
+			}
 		}
-		*/
+	}
+
+	public setCurse(isCursed:Boolean){
+		curse = isCursed;
+	}
+
+	public toggleCurse(){
+		curse = !curse;
 	}
 }
