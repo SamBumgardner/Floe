ee26c22ed4af3ea48e8b5c427eb7cf88d8d86bcb
Noah Bumgardner <noah@playgamecafe.com>
2016-04-17 19:56:16 -0500

    Whitespace changes. Converted leading spaces into tabs, fixed indentation, removed trailing spaces.

--- Floe/src/entities/HUD.hx
+++ Floe/src/entities/HUD.hx
@@ -5,19 +5,19 @@ import com.haxepunk.graphics.Text;
 import com.haxepunk.graphics.Image;
 
 class HUD extends Entity {
-    private var textScore = new Text("000000000", 470, 15, 0, 0);
-    private var textLevel = new Text("Lake 1", 260, 15, 0, 0);
+	private var textScore = new Text("000000000", 470, 15, 0, 0);
+	private var textLevel = new Text("Lake 1", 260, 15, 0, 0);
 	private var textHealth = new Text("x 3", 75, 15, 0, 0);
-	
+
 	// Heart images
 	private var imgHeart = new Image("graphics/heart.png");
 	private var imgHeartGreen = new Image("graphics/heartGreen.png");
-	
-	// Constructor
-    public function new(x:Int, y:Int, startHP:Int){
-        super(x, y);
 
-        textScore.size = 30;
+	// Constructor
+	public function new(x:Int, y:Int, startHP:Int){
+		super(x, y);
+
+		textScore.size = 30;
 		textScore.align = RIGHT;
 		textLevel.size = 30;
 		textHealth.size = 30;
@@ -25,29 +25,29 @@ class HUD extends Entity {
 		addGraphic(textScore);
 		addGraphic(textLevel);
 		addGraphic(textHealth);
-		
+
 		imgHeart.scale = 1.70;
 		addGraphic(imgHeart);
-    }
+	}
 
-    public function updateScore(score:Int):Void{
-        // Add leading zeroes to score
+	public function updateScore(score:Int):Void{
+		// Add leading zeroes to score
 		var zeroes:String = "";
 		var scoreString:String = Std.string(score);
 		for (i in 0...(9-scoreString.length)){
-			    zeroes += "0";
+	    zeroes += "0";
 		}
-        textScore.text = zeroes + scoreString;
-    }
+		textScore.text = zeroes + scoreString;
+	}
 
-    public function updateLake(lakeID:Int):Void{
-        textLevel.text = "Lake " + Std.string(lakeID);
-    }
+	public function updateLake(lakeID:Int):Void{
+		textLevel.text = "Lake " + Std.string(lakeID);
+	}
 
-    public function updateHealth(hp:Int):Void{
-        textHealth.text = "x " + Std.string(hp);
-    }
-	
+	public function updateHealth(hp:Int):Void{
+		textHealth.text = "x " + Std.string(hp);
+	}
+
 	/* ONLY IMPLEMENT WHEN ALL ELSE IS WORKING
 	public function setCurse(status:Bool){
 		if (status){
