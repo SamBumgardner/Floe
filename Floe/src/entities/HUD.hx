db290d0653e2d06c82e955f19225c099190c6938
Noah Bumgardner <noah@playgamecafe.com>
2016-04-17 22:30:21 -0500

    Change score text offset. Add background image for the HUD, on layer 100. Shrink heart. Redo adding of graphics; takes place in function add(), which is called each time the object is added into the scene. Shift heart 7 pixels away from the top-left corner. *Change was received from Sam Bumgardner, then altered with whitespace.*

--- Floe/src/entities/HUD.hx
+++ Floe/src/entities/HUD.hx
@@ -1,17 +1,22 @@
 package entities;
 
 import com.haxepunk.Entity;
+import com.haxepunk.HXP;
 import com.haxepunk.graphics.Text;
 import com.haxepunk.graphics.Image;
 
 class HUD extends Entity {
-	private var textScore = new Text("000000000", 470, 15, 0, 0);
+	private var textScore = new Text("000000000", 465, 15, 0, 0);
 	private var textLevel = new Text("Lake 1", 260, 15, 0, 0);
 	private var textHealth = new Text("x 3", 75, 15, 0, 0);
 
 	// Heart images
 	private var imgHeart = new Image("graphics/heart.png");
 	private var imgHeartGreen = new Image("graphics/heartGreen.png");
+
+	// Background image
+
+	private var imgBackground = new Image("graphics/levelBackground.png");
 
 	// Constructor
 	public function new(x:Int, y:Int, startHP:Int){
@@ -22,12 +27,16 @@ class HUD extends Entity {
 		textLevel.size = 30;
 		textHealth.size = 30;
 
-		addGraphic(textScore);
-		addGraphic(textLevel);
-		addGraphic(textHealth);
+		imgHeart.scale = 1.5;
+	}
 
-		imgHeart.scale = 1.70;
-		addGraphic(imgHeart);
+	public override function added(){
+		HXP.scene.addGraphic(textScore);
+		HXP.scene.addGraphic(textLevel);
+		HXP.scene.addGraphic(textHealth);
+
+		HXP.scene.addGraphic(imgHeart, 0, 7, 7);
+		HXP.scene.addGraphic(imgBackground, 100, 0, 0);
 	}
 
 	public function updateScore(score:Int):Void{
@@ -35,7 +44,7 @@ class HUD extends Entity {
 		var zeroes:String = "";
 		var scoreString:String = Std.string(score);
 		for (i in 0...(9-scoreString.length)){
-	    zeroes += "0";
+			zeroes += "0";
 		}
 		textScore.text = zeroes + scoreString;
 	}
