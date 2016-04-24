cd86c61c2fefe4db5a8b11c70d1b6460f6a72565
Sam Bumgardner <sambumgardner1@gmail.com>
2016-04-23 20:21:05 -0500

    Added documentation to Main.

--- Floe/src/Main.hx
+++ Floe/src/Main.hx
@@ -10,6 +10,11 @@ class Main extends Engine
 	public var musicVolume:Float = .5;
 	public var sfxVolume:Float = .25;
 
+	
+	// init()
+	//
+	// Initializes the game, creating the first MainMenu scene.
+	
 	override public function init()
 	{
 #if debug
@@ -19,13 +24,27 @@ class Main extends Engine
 		HXP.scene = new scenes.MainMenu();
 	}
 
+	
+	// main()
+	//
+	// Calls Main's constructor, setting everything into motion.
+	
 	public static function main() { new Main(); }
 	
+	
+	// new()
+	//
+	// Main's constructor. Simply passes off values to its parent class' constructor.
 	
 	public function new(width:Int = 0, height:Int = 0)
 	{
 		super(width, height, 60, false);
 	}
+	
+	
+	// returnToMenu()
+	//
+	// Makes MainMenu the current scene.
 	
 	public function returnToMenu(){
 		//HXP.console.log(["Returning to main menu..."]);
@@ -33,6 +52,12 @@ class Main extends Engine
 		
 		HXP.scene = new scenes.MainMenu();
 	}
+	
+	
+	// startGame()
+	//
+	// Randomizes HXP's RNG seed, creates a GameManager,
+	// and makes an instance of GameScene the active scene.
 	
 	public function startGame(seed:String){
 		// --- Sets up manager and RNG for the new game ---
@@ -51,14 +76,24 @@ class Main extends Engine
 		HXP.scene = new scenes.GameScene(gm);
 	}
 	
+	
+	// nextLevel()
+	// 
+	// Makes a new GameScene the active scene.
+	
 	public function nextLevel(){
 		HXP.scene = new scenes.GameScene();
 	}
 	
+	
+	// gameOver()
+	//
+	// Makes a new GameOver scene the active scene.
+	
 	public function gameOver(){
 
 		//HXP.console.log(["Game Over!"]);
-		var gm:GameManager = (cast HXP.scene).gameOver(); //ends the looping music.
+		var gm:GameManager = (cast HXP.scene).gameOver(); 
 		HXP.scene = new scenes.GameOver(gm);
 	
 	}
